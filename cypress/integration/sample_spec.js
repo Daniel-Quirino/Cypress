d637538287d3ac9227183cf8ecd0924953720174
Daniel Pires Quirino <danielpires6@hotmail.com>
2019-05-31 06:00:20 -0300

    Pegar o primeiro elemento de uma div que possui a TAG de marcação input

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,8 +2,12 @@ describe('My First Test', function() {
     it('clicking "type" shows the right headings', function() {
       cy.visit('https://example.cypress.io/commands/querying')
   
-      cy.get('.query-list')
-        .contains('apples')
-        .should('have.class', 'first')
+      cy.get('.query-form').within(() => {
+        cy.get('input:first')
+        .should('have.attr', 'placeholder')
+        cy.get('input:last')
+        .should('have.attr', 'placeholder', 'Password')
+      })
+
     })
   })
